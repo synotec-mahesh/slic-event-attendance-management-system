84eaa7faddd2c8e4f7a674291de2c333ce4d30b0
SupunSynotec <supun.synotec@gmail.com>
2022-12-09 11:14:34 +0530

    #13 Admin Dashboard Design

--- resources/views/backend/layouts/inc/footer.blade.php
+++ resources/views/backend/layouts/inc/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
-    <strong>Copyright &copy;2022 <a href="https://www.synotec.lk/">synotec</a>.</strong>
-    All rights reserved.
+    {{-- <strong>Copyright &copy;2022 <a href="https://www.synotec.lk/">synotec</a>.</strong>
+    All rights reserved. --}}
     <div class="float-right d-none d-sm-inline-block">
      
     </div>
